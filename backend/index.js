bb5961ef9d6b72668b5ad0160bc2bce5c1059f94
elopez <ediloaz@gmail.com>
2025-02-15 10:45:12 -0600

    add more gets to test vercel

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,22 @@ app.get('/', (req, res) => {
   res.send('Bienvenido al API del backend.');
 });
 
+app.get('/api', (req, res) => {
+  res.send('Bienvenido al API del backend.');
+});
+
+app.get('/api/1', (req, res) => {
+  res.send('Bienvenido al API del backend.');
+});
+
+app.get('/test', (req, res) => {
+  res.json({
+    timestamp: Date.now(),
+    texto: "Hola Mundo",
+  });
+});
+
+
 app.get('/api/test', (req, res) => {
   res.json({
     timestamp: Date.now(),
