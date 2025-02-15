cf055d8b429db00f2c2a1279d53d17697b698060
elopez <ediloaz@gmail.com>
2025-02-15 10:56:05 -0600

    remove unused API endpoints from backend

--- backend/index.js
+++ backend/index.js
@@ -13,29 +13,12 @@ app.get('/', (req, res) => {
   res.send('Bienvenido al API del backend.');
 });
 
-app.get('/api', (req, res) => {
-  res.send('Bienvenido al API del backend.');
-});
-
-app.get('/api/1', (req, res) => {
-  res.send('Bienvenido al API del backend.');
-});
-
-app.get('/test', (req, res) => {
-  res.json({
-    timestamp: Date.now(),
-    texto: "Hola Mundo",
-  });
-});
-
-
 app.get('/api/test', (req, res) => {
   res.json({
     timestamp: Date.now(),
     texto: "Hola Mundo",
   });
 });
-
 
 app.get('/api/info', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
